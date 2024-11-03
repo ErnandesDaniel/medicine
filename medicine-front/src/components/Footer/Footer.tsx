684b92e031e502cffa22e051153b44321d0cd9c6
Ernandes <Ernandes20@yandex.ru>
2024-11-03 11:58:48 +0300

    Исправлен footer

--- medicine-front/src/components/Footer/Footer.tsx
+++ medicine-front/src/components/Footer/Footer.tsx
@@ -5,14 +5,20 @@ import Text from "@/components/Text";
 export default function Footer() {
     return (
         <div className='footer'>
+            <Spacer space={30} />
             <Text>MEDML MEPHI</Text>
-            <Spacer space={20} />
+            <Spacer space={30} />
             <Text>Адрес:</Text>
             <Text>Москва, Каширское ш. 31</Text>
-            <Spacer space={10} />
+            <Spacer space={20} />
             <Text>Контакты:</Text>
             <Text>1800 123 4567</Text>
             <Text>info@innovativemedhtech.com</Text>
+            <Spacer space={20} />
+            <div className='line'></div>
+            <Spacer space={10} />
+            <Text className='under_line'>2024 MEDML MEPHI</Text>
+            <Spacer space={30} />
         </div>)
 }
 
